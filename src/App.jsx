fa1dea80326b5ac88d36c4b73f7c0d8b8a47136d
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-05 07:04:29 +0000

    adds username random color fuctionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import Messagelist from './Messagelist.jsx';
 
+var colorArray = [ {color: '#FE9393'}, {color: '#FFDDA3'}, {color: '#C0F5C6'}, {color: '#C0F2F5'}, {color: '#CDC0F5'}]
+var randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +13,7 @@ class App extends Component {
         currentUser: {name: "Anonymous"},
         messages: []
         },
-      userCount: 0
+      userCount: 0,
     }
     this.Socket = new WebSocket("ws://localhost:3001");
     console.log(this.Socket.protocol);
@@ -21,17 +23,16 @@ class App extends Component {
     this.handleMessage = this.handleMessage.bind(this);
     this.changeUser = this.changeUser.bind(this);
     } 
-  
+
   handleMessage = function (event) {
     let messageObj = JSON.parse(event.data);
-    console.log(event.data)
     if(messageObj.type === "usercount") {
       this.setState((prevState) => {
         Object.assign(prevState, {userCount: messageObj.users})
       });
     }
     let type = messageObj.type
-    let incomingMessage = {id: messageObj.id, username: messageObj.username, content: messageObj.content, type: type};
+    let incomingMessage = {id: messageObj.id, username: messageObj.username, content: messageObj.content, type: type, color: messageObj.color};
     let messageConcat = this.state.data.messages.concat(incomingMessage);
     this.setState({data: { currentUser: this.state.data.currentUser, messages: messageConcat}})
   }
@@ -47,7 +48,8 @@ class App extends Component {
       const messageObject = {
         type: "postMessage",
         content: e.target.value,
-        username: this.state.data.currentUser.name
+        username: this.state.data.currentUser.name,
+        color: randomColor
       }
       this.Socket.send(JSON.stringify(messageObject)); 
       console.log(`SENT "${e.target.value}" TO SERVER!`)
@@ -78,7 +80,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
           <a className="usercounter">Users Online =  {this.state.userCount}</a>
         </nav>
-        <Messagelist messages={this.state.data.messages}/>
+        <Messagelist messages={this.state.data.messages} currentUser={this.state.data.currentUser}/>
         <ChatBar currentUser={this.state.data.currentUser} onKeyPress={this.inputMessage} onUsernameUpdate={this.changeUser}/>
       </div>
     );
--- src/Messagelist.jsx
+++ src/Messagelist.jsx
@@ -6,7 +6,7 @@ class Messagelist extends Component {
     render() {
       const message = this.props.messages.map((message) => {
           if (message.type === "incomingMessage"){
-            return <Message singleMessage={message.content} key={message.id} username={message.username} type={message.type}/>
+            return <Message singleMessage={message.content} key={message.id} username={message.username} type={message.type} color={message.color}/>
           }
           else if (message.type === "incomingNotification"){
             return <Notification singleNotification={message.content} key={message.id} />
